8ad9c533c74f12a8616416e168fd08c627da7137
Mori <laurent.ferric@gmail.com>
2024-12-21 12:19:11 +0100

    added Service class that handles the business logic behind the API endpoints. This class will call the different repository classes that connect to the DB. ServerController will now only define the endpoints and bridge them to the ServerService class for processing.

--- src/main/java/fr/laurentFE/todolistspringbootserver/web/ServerController.java
+++ src/main/java/fr/laurentFE/todolistspringbootserver/web/ServerController.java
@@ -1,26 +1,25 @@
 package fr.laurentFE.todolistspringbootserver.web;
 
 import fr.laurentFE.todolistspringbootserver.model.User;
-import fr.laurentFE.todolistspringbootserver.repository.UsersRepository;
+import fr.laurentFE.todolistspringbootserver.service.ServerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/* This class handles the REST APIs */
 @RestController
 public class ServerController {
 
-    private final UsersRepository usersRepository;
-
-    public ServerController(UsersRepository userRepository) {
-        this.usersRepository = userRepository;
-    }
+    @Autowired
+    private ServerService serverService;
 
     @GetMapping("/")
     public String displayDocumentation() {
         return "list available endpoints and usage";
     }
 
-    @GetMapping("/rest/Users/")
+    @GetMapping("/rest/Users")
     public Iterable<User> getUsers() {
-        return usersRepository.findAll();
+        return serverService.findAllUsers();
     }
 }
