0d06af6e18010d53932c95253497519e264be9e5
Mori <laurent.ferric@gmail.com>
2024-12-22 17:39:26 +0100

    added endpoints GET /rest/Users PUT rest/Users/{id} and POST rest/Users

--- src/main/java/fr/laurentFE/todolistspringbootserver/service/ServerService.java
+++ src/main/java/fr/laurentFE/todolistspringbootserver/service/ServerService.java
@@ -1,9 +1,14 @@
 package fr.laurentFE.todolistspringbootserver.service;
 
 import fr.laurentFE.todolistspringbootserver.model.User;
+import fr.laurentFE.todolistspringbootserver.model.UserMapper;
 import fr.laurentFE.todolistspringbootserver.repository.UsersRepository;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /* This class handles the business logic behind the API endpoints */
 @Service
@@ -12,7 +17,57 @@ public class ServerService {
     @Autowired
     private UsersRepository usersRepository;
 
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    private boolean userNameExists(String user_name) {
+        List<User> users = jdbcTemplate.query(
+                "SELECT * FROM users WHERE user_name=:name",
+                    new MapSqlParameterSource().addValue("name", user_name),
+                    new UserMapper());
+
+        return !users.isEmpty();
+    }
+
     public Iterable<User> findAllUsers() {
         return usersRepository.findAll();
+    }
+
+    public User findUser(Integer id) {
+        return usersRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        if (userNameExists(user.getUser_name())) {
+            user.setUser_id(-409);
+            user.setUser_name("user_name already exists");
+            return user;
+        }
+        else if (user.getUser_id() != null) {
+            user.setUser_id(-400);
+            user.setUser_name("user_id must not be provided");
+            return user;
+        }
+        else {
+            return usersRepository.save(user);
+        }
+    }
+
+    public User updateUser(User user, Integer id) {
+        if (user.getUser_id() != null) {
+            user.setUser_id(-400);
+            user.setUser_name("user_id must not be provided in request body");
+            return user;
+        }
+        User previous_user = findUser(id);
+        if (previous_user == null) {
+            user.setUser_id(-404);
+            user.setUser_name("requested user_id not found");
+            return user;
+        }
+        else {
+            user.setUser_id(id);
+            return usersRepository.save(user);
+        }
     }
 }
--- src/main/java/fr/laurentFE/todolistspringbootserver/web/ServerController.java
+++ src/main/java/fr/laurentFE/todolistspringbootserver/web/ServerController.java
@@ -3,8 +3,9 @@ package fr.laurentFE.todolistspringbootserver.web;
 import fr.laurentFE.todolistspringbootserver.model.User;
 import fr.laurentFE.todolistspringbootserver.service.ServerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 /* This class handles the REST APIs */
 @RestController
@@ -18,8 +19,38 @@ public class ServerController {
         return "list available endpoints and usage";
     }
 
+
+    /*
+     * User endpoints
+     */
     @GetMapping("/rest/Users")
     public Iterable<User> getUsers() {
         return serverService.findAllUsers();
+    }
+
+    @GetMapping("/rest/Users/{id}")
+    public User getUser(@PathVariable Integer id) {
+        User user = serverService.findUser(id);
+        if (user == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return user;
+    }
+
+    @PostMapping("/rest/Users")
+    public User createUser(@RequestBody User user) {
+        User new_user = serverService.createUser(user);
+        if (new_user.getUser_id() < 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, user.getUser_name());
+        }
+        return new_user;
+    }
+
+    @PutMapping("/rest/Users/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable Integer id) {
+        System.out.println(id);
+        User updated_user = serverService.updateUser(user, id);
+        if (updated_user.getUser_id() < 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, user.getUser_name());
+        }
+        return updated_user;
     }
 }
